c1059604249a0863d75b0240312bc612b904c4a2
valxmachi <valxmachi@gmail.com>
2025-11-27 00:48:13 +0000

    fix layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,22 @@
+
 import type { Metadata } from "next";
 import { Dosis, IBM_Plex_Mono, Kaushan_Script } from "next/font/google";
+import dynamic from "next/dynamic";
 import "./globals.css";
+
 import { ThemeProvider } from "@/hooks/use-theme";
-import { SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/layout/AppSidebar";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
 import AppMain from "@/components/layout/AppMain";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { FirebaseClientProvider } from "@/firebase/client-provider";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import Header from "@/components/layout/Header";
+
+const FirebaseClientProvider = dynamic(
+  () => import("@/firebase/client-provider").then((mod) => mod.FirebaseClientProvider),
+  { ssr: false }
+);
 
 const dosis = Dosis({ subsets: ["latin"], variable: "--font-dosis" });
 const ibmPlexMono = IBM_Plex_Mono({
@@ -41,16 +49,22 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased flex",
           dosis.variable,
           ibmPlexMono.variable,
-          kaushanScript.variable,
+          kaushanScript.variable
         )}
       >
-        <ThemeProvider storageKey="skomidora-theme" defaultTheme="dark">
+        <ThemeProvider
+          storageKey="skomidora-theme"
+          defaultTheme="dark"
+        >
           <FirebaseClientProvider>
             <TooltipProvider>
               <SidebarProvider>
                 <div className="flex flex-1">
                   <AppSidebar />
-                  <AppMain>{children}</AppMain>
+                  <div className="flex flex-col flex-1 min-h-screen">
+                    <Header />
+                    <AppMain>{children}</AppMain>
+                  </div>
                 </div>
                 <Toaster />
               </SidebarProvider>
